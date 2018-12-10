2a0d3e6cc324d6f88cc784ab4ed3cfda1670c489
JBT <jbtobar.io@gmail.com>
2018-12-10 17:22:39 -0500

    amplify push

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,7 +4,11 @@ const awsmobile =  {
     "aws_project_region": "us-east-1",
     "aws_content_delivery_bucket": "dapployer-20181210171442--hostingbucket",
     "aws_content_delivery_bucket_region": "us-east-1",
-    "aws_content_delivery_url": "http://dapployer-20181210171442--hostingbucket.s3-website-us-east-1.amazonaws.com"
+    "aws_content_delivery_url": "http://dapployer-20181210171442--hostingbucket.s3-website-us-east-1.amazonaws.com",
+    "aws_cognito_identity_pool_id": "us-east-1:a449bea6-633f-42c1-bf1e-b950acca6fd4",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_DQSLfBKg1",
+    "aws_user_pools_web_client_id": "5j0vubgk3nh02ah8johm48uvqs"
 };
 
 
