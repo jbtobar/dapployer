f25801665ea2c5b6eb750b9d91ff2812de7dac87
JBT <jbtobar.io@gmail.com>
2018-12-10 17:24:19 -0500

    withauthenticator

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
@@ -25,4 +30,5 @@ class App extends Component {
   }
 }
 
-export default App;
+// export default App;
+export default withAuthenticator(App, true);
