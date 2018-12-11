cca983b6068befcad3fa8ce3d8a356c2dc92a3f0
JBT <jbtobar.io@gmail.com>
2018-12-11 06:51:21 -0500

    storage list

--- src/screens/itemDashboard.js
+++ src/screens/itemDashboard.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import {Container, Card} from 'semantic-ui-react'
+import {Container, Card,Button} from 'semantic-ui-react'
 import CreateItemModal from './createItem'
+import { Storage } from 'aws-amplify';
 
 class ItemDashboard extends Component {
+
+
+lister = () => {
+  Storage.list('')
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
+}
 render() {
     return (
       <div>
        <CreateItemModal/>
         <Container style={{padding: 10}}>
+        <Button onClick={this.lister}>Lister</Button>
           <Card.Group>
               <Card>
               <Card.Content>
